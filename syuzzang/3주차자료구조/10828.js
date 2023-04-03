a7e7ab0beec1b46b527554d02f59a96098f24b70
[IoT1]이수연 <dudgk0216@gmail.com>
2023-04-03 09:50:21 +0900

    solve 17478

--- syuzzang/3주차자료구조/10828.js
+++ syuzzang/3주차자료구조/10828.js
@@ -1,4 +1,56 @@
-const array = require("fs").readFileSync("./input.txt").toString().split("\n");
+// let fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+// const [n, ...rest] = input;
+
+// function solution(n, commands) {
+//   const stack = [];
+//   let top = 0;
+//   //   명령어에 따른 인덱스길이의 변화를 변수에 담아둔다
+//   let answer = "";
+
+//   for (let i = 0; i < n; i += 1) {
+//     const command = commands[i].split(" ")[0];
+//     let result = "";
+
+//     switch (command) {
+//       case "push":
+//         let pushItem = commands[i].split(" ")[1];
+//         stack[top++] = pushItem;
+//         break;
+//       case "pop":
+//         if (top) {
+//           top -= 1;
+//           result = stack.splice(-1);
+//           answer += result + " ";
+//         } else {
+//           result = -1;
+//           answer += result + " ";
+//         }
+//         break;
+//       case "top":
+//         result = top ? stack[top - 1] : -1;
+//         answer += result + " ";
+//         break;
+//       case "empty":
+//         result = top < 1 ? 1 : 0;
+//         answer += result + " ";
+//         break;
+//       case "size":
+//         result = top;
+//         answer += result + " ";
+//         break;
+
+//       default:
+//         break;
+//     }
+//   }
+//   return answer.split(" ").join("\n").trim();
+// }
+
+// console.log(solution(n, rest));
+
+const array = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 array.shift();
 
 const stack = [];
@@ -21,4 +73,10 @@ const result = array.reduce(
 
 console.log(result);
 
-// 원래 일일이 각 메서드들을 직접 만들어서 구현했으나 본 풀이법이 더 효율적일것같아 공부해봤다.
+// 기존 로직에 비해 80ms 더빠르고 메모리 사용도 1000kb 차이가 난다.
+// push 1
+// push 2
+// top
+// size
+// empty
+// pop
